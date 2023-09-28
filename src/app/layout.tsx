2df357479065cd0b57da9dce9f6354724058a8fc
Pedro Valério <pedroddjkK@gmail.com>
2023-09-28 14:36:19 -0300

    Atualizando Next e Mudando Titulo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,11 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 
-const roboto = Roboto({ weight: ["400", "700"], subsets: ["latin"]});
+const roboto = Roboto({ weight: ["400", "700"], subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "ApexAI",
-  description: "Inteligência Artificial para o seu negocio!",
+  title: "InhaLock",
+  description: "InhaLock - O melhor sistema de controle de chaves",
 };
 
 export default function RootLayout({
